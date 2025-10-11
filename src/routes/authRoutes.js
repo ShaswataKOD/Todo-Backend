1edd546e166dc980cc385e179e2c73c80caf8ca9
Shaswata Biswas <shaswata@itobuz.com>
2025-10-11 18:39:20 +0530

    WIP:authentication-routes

---
+++ src/routes/authRoutes.js
@@ -0,0 +1,6 @@
+ import express from "express"
+ import bcrypt from "bcryptjs"
+ import jwt from json  
+ const router = express.Router();
+ import User from "../models/authModel"
+ const bcrypt = require('bcrypt');
