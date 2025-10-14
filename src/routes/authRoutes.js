e2904980a9eeda7d74288aa1690f9d7b769ffb34
Shaswata Biswas <shaswata@itobuz.com>
2025-10-14 18:57:40 +0530

    wip:loginPage

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express'
 import { registerUser } from '../controllers/authController.js'
 import { resetPassword, sendOtp, VerifyOtp } from '../controllers/otpController.js'
+import  {verifyToken} from  "../Middleware/authValidator.js"
 
 const router = express.Router()
 
 router.post('/register', registerUser) 
+router.post("/login",verifyToken,login)
 router.post('/send-otp', sendOtp) 
 router.post('/verify-otp', VerifyOtp) 
 router.post("/reset-password",resetPassword)
-// router.post("forget-password",forgetPassword)
+
 
 export default router
